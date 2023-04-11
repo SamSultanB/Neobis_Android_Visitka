d6be66392c9914cdd988fbae1297e7ce75406ef1
lux <ssbekmuratov@gmail.com>
2023-04-12 00:13:35 +0600

    second UI page

--- app/src/main/java/com/example/businesscard/MainActivity.kt
+++ app/src/main/java/com/example/businesscard/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.businesscard
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.businesscard.databinding.ActivityMainBinding
@@ -10,5 +11,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.button1.setOnClickListener {
+            var intent = Intent(this, Activity2::class.java)
+            startActivity(intent)
+        }
     }
 }
