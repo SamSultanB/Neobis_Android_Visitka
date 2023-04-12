fcb743be6bae749fb10eed4077a340d4938d5c9f
lux <ssbekmuratov@gmail.com>
2023-04-12 22:54:55 +0600

    intent actions

--- app/src/main/java/com/example/businesscard/Activity2.kt
+++ app/src/main/java/com/example/businesscard/Activity2.kt
@@ -1,7 +1,12 @@
 package com.example.businesscard
 
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.InetAddresses
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.businesscard.databinding.Activity2Binding
 
 class Activity2 : AppCompatActivity() {
@@ -12,5 +17,52 @@ class Activity2 : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = Activity2Binding.inflate(layoutInflater)
         setContentView(binding.root)
+        var intent = intent
+        binding.imageDoctor.setImageResource(intent.getIntExtra("photo", 0))
+        binding.textName.setText(intent.getStringExtra("name"))
+        binding.textProf.setText(intent.getStringExtra("speciality"))
+        binding.callCard.setOnClickListener { callAction("tel: 0557130028") }
+        binding.emailCard.setOnClickListener { gmailAction("mailto:someone@gmail.com") }
+        binding.webSiteCard.setOnClickListener { websiteAction("http://www.google.com") }
+        binding.map.setOnClickListener { mapAction("geo:7003007619951323774") }
+
     }
+
+    fun callAction(uri: String){
+        try {
+            var call = Intent(Intent.ACTION_DIAL, Uri.parse(uri))
+            startActivity(call)
+        }catch (e: ActivityNotFoundException){
+            Toast.makeText(this, "Something went wrong", Toast.LENGTH_SHORT)
+        }
+    }
+
+    fun gmailAction(uri: String){
+        try {
+            var gmail = Intent(Intent.ACTION_SENDTO, Uri.parse(uri))
+            startActivity(gmail)
+        }catch (e: ActivityNotFoundException){
+            Toast.makeText(this, "Upss! Something went wrong!", Toast.LENGTH_SHORT)
+        }
+    }
+
+    fun websiteAction(uri: String){
+        try {
+            var website = Intent(Intent.ACTION_VIEW, Uri.parse(uri))
+            startActivity(website)
+        }catch (e: ActivityNotFoundException){
+            Toast.makeText(this, "Nooo", Toast.LENGTH_SHORT)
+        }
+    }
+
+    fun mapAction(uri: String){
+        try {
+            var map = Intent(Intent.ACTION_VIEW, Uri.parse(uri))
+            map.setPackage("com.google.android.apps.maps")
+            startActivity(map)
+        }catch (e: ActivityNotFoundException){
+            Toast.makeText(this, "Can't open it", Toast.LENGTH_SHORT)
+        }
+    }
+
 }
--- app/src/main/java/com/example/businesscard/MainActivity.kt
+++ app/src/main/java/com/example/businesscard/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.businesscard
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract.DisplayPhoto
 import com.example.businesscard.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,9 +12,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.button1.setOnClickListener {
-            var intent = Intent(this, Activity2::class.java)
-            startActivity(intent)
-        }
+        binding.button1.setOnClickListener { passData("Dr. Novikov","Dentist", R.drawable.doctor1)}
+        binding.button2.setOnClickListener { passData("Dr. Nurmatov", "Surgeon", R.drawable.doctor2) }
+        binding.button3.setOnClickListener { passData("Dr. Khalilova", "Oculist", R.drawable.doctor3) }
+        binding.button4.setOnClickListener { passData("Dr. Alieva ", "Nurse", R.drawable.doctor4) }
+        binding.button5.setOnClickListener { passData("Dr. Kolesova", "Doctor", R.drawable.doctor5) }
+    }
+
+    fun passData(name: String, speciality: String, photo: Int){
+        var intent = Intent(this, Activity2::class.java)
+        intent.putExtra("name", name)
+        intent.putExtra("speciality", speciality)
+        intent.putExtra("photo", photo)
+        startActivity(intent)
     }
 }
